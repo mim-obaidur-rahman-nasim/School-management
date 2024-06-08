fec54c1d82cf6478a276b64be572c9ea12630a6c
Mim Obaidur Rahman Nasim <obaidur150@gmail.com>
2024-06-08 23:14:11 +0600

    first commit

---
+++ resources/views/components/sideNavBar.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Side Navbar</title>
+    <link rel="stylesheet" href="{{ asset('css/sideNavBar.css') }}">
+<link rel="stylesheet" href="{{ asset('fontawesome-free-6.5.2-web/css/all.min.css') }}">
+</head>
+<body>
+    <div class="sidebar">
+        <a href="">Borna</a>
+        <a href="">Joya</a>
+        <a href="">Toma</a>
+        <a href="">Riddhi</a>
+    </div>
+</body>
+</html>
---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome-free-6.5.2-web/css/all.min.css') }}">
+</head>
+<body>
+    <!-- <div class="check">
+        <div class="first">hello</div>
+        <div class="second">world</div>
+    </div> -->
+    <div class="check">
+    <x-sideNavBar />
+    <x-topNavBar />
+    </div>
+
+</body>
+</html>
